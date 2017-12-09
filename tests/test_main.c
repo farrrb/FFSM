43954eccb83e07f1fb196cb42eb61aa29ec4c10c
Fabian Zahn <fabian.zahn@gmail.com>
2017-12-09 15:59:52 +0100

    Unity integration

--- tests/test_main.c
+++ tests/test_main.c
@@ -1,13 +1,31 @@
 //
 // Created by farrrb on 09-Dec-17.
 //
-#include <stdio.h>
 
+#include "unity.h"
 #include "fbpfsm.h"
 
-int main(int argc, char* argv[])
+// test: setup
+void setUp(void)
 {
-  printf("Test: %d\n", my_test_func (23));
+}
 
-  return 0;
+// test: tear down
+void tearDown(void)
+{
+}
+
+// the actual test
+void test_Template(void)
+{
+  TEST_ASSERT_EQUAL(11, my_test_func(23));
+}
+
+int main(int argc, char *argv[])
+{
+  UNITY_BEGIN();
+
+  RUN_TEST(test_Template);
+
+  return UNITY_END();
 }
