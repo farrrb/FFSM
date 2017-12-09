0134bc04de84456c8050b84bcdee6c277a7f23c2
Fabian Zahn <fabian.zahn@gmail.com>
2017-12-09 23:18:29 +0100

    Added some more code coverage (hopefully)

--- tests/test_main.c
+++ tests/test_main.c
@@ -16,9 +16,9 @@ struct fsm_data fsm_test_data;
 
 #define FFSM_SIG_TRANS_TO_NONE 1
 
-void *test_init_state(int signal, void * data)
+void *test_init_state(FFSM_Event_t event, void * data)
 {
-  switch (signal)
+  switch (event)
   {
     case FFSM_SIG_NONE:
     {
@@ -43,6 +43,13 @@ void *test_init_state(int signal, void * data)
   }
 }
 
+void *test_final_state(FFSM_Event_t event, void * data)
+{
+  (void)event;
+  (void)data;
+}
+
+
 // test: setup
 void setUp(void)
 {
@@ -62,6 +69,10 @@ void test_init(void)
 
   FFSM_sendSignal(&fsm_test, FFSM_SIG_TRANS_TO_NONE);
   TEST_ASSERT_EQUAL_PTR(fsm_test.current_state, FFSM_STATE_NONE);
+
+  FFSM_transit(&fsm_test, test_final_state);
+  TEST_ASSERT_EQUAL_PTR(fsm_test.current_state, test_final_state);
+
 }
 
 int main(int argc, char *argv[])
