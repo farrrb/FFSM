727e4f2159c4f4b896c6772451af944aad7deb2f
Fabian Zahn <fabian.zahn@gmail.com>
2017-12-11 07:09:28 +0100

    - Some code coverage

--- tests/test_stack.c
+++ tests/test_stack.c
@@ -7,7 +7,7 @@
 #include "FFSM_Stack.h"
 
 #define TEST_STACK_SIZE (3)
-FFSM_Stack_t test_stack;
+FFSM_Stack_t test_stack; ///< my test stack
 FFSM_State_t test_data[TEST_STACK_SIZE] = {(FFSM_State_t)0xF, (FFSM_State_t)0xA, (FFSM_State_t)0xB};
 
 // test: setup
@@ -23,7 +23,11 @@ void tearDown(void)
 // the actual test
 void test_init(void)
 {
-  int ret = -1;
+  int ret;
+
+  ret = FFSM_Stack_init((FFSM_Stack_t *)0, (FFSM_State_t *)0, 0);
+  TEST_ASSERT_EQUAL(ret, -1);
+
   TEST_ASSERT_EQUAL_PTR((FFSM_State_t)0xF, test_data[0]);
   TEST_ASSERT_EQUAL_PTR((FFSM_State_t)0xA, test_data[1]);
   TEST_ASSERT_EQUAL_PTR((FFSM_State_t)0xB, test_data[2]);
