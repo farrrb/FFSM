f0d66150ac0693550aa3f7965c57cd0b2f64496f
Fabian Zahn <fabian.zahn@gmail.com>
2017-12-23 11:57:37 +0100

    Changed parameter of test functions from actual, excpected to -> expected, actual

--- tests/test_stack.c
+++ tests/test_stack.c
@@ -43,41 +43,41 @@ void test_init(void)
   ret = FFSM_Stack_init(&test_stack, test_data, TEST_STACK_SIZE, test_root);
   TEST_ASSERT_EQUAL(ret, 0);
 
-  TEST_ASSERT_EQUAL(test_stack.size, TEST_STACK_SIZE);
-  TEST_ASSERT_EQUAL(test_stack.top, 1);
-  TEST_ASSERT_EQUAL(test_stack.data, test_data);
+  TEST_ASSERT_EQUAL(TEST_STACK_SIZE, test_stack.size);
+  TEST_ASSERT_EQUAL(1,               test_stack.top);
+  TEST_ASSERT_EQUAL(test_data,       test_stack.data);
 
-  TEST_ASSERT_EQUAL_PTR(test_root, test_data[0]);
+  TEST_ASSERT_EQUAL_PTR(test_root,       test_data[0]);
   TEST_ASSERT_EQUAL_PTR(FFSM_STATE_NONE, test_data[1]);
   TEST_ASSERT_EQUAL_PTR(FFSM_STATE_NONE, test_data[2]);
 
 
   // push tests
-  TEST_ASSERT_EQUAL(FFSM_Stack_push(&test_stack, (FFSM_State_t)1), 0);
-  TEST_ASSERT_EQUAL(test_stack.top, 2);
-  TEST_ASSERT_EQUAL_PTR(test_stack.data[1], (FFSM_State_t)1);
+  TEST_ASSERT_EQUAL(                  0, FFSM_Stack_push(&test_stack, (FFSM_State_t)1));
+  TEST_ASSERT_EQUAL(                  2, test_stack.top);
+  TEST_ASSERT_EQUAL_PTR((FFSM_State_t)1, test_stack.data[1]);
 
-  TEST_ASSERT_EQUAL(FFSM_Stack_push(&test_stack, (FFSM_State_t)2), 0);
-  TEST_ASSERT_EQUAL_PTR(test_stack.top, 3);
-  TEST_ASSERT_EQUAL_PTR(test_stack.data[2], (FFSM_State_t)2);
+  TEST_ASSERT_EQUAL(                  0, FFSM_Stack_push(&test_stack, (FFSM_State_t)2));
+  TEST_ASSERT_EQUAL_PTR(              3, test_stack.top);
+  TEST_ASSERT_EQUAL_PTR((FFSM_State_t)2, test_stack.data[2]);
 
-  TEST_ASSERT_EQUAL(FFSM_Stack_push(&test_stack, (FFSM_State_t)3), -1);
-  TEST_ASSERT_EQUAL(test_stack.top, 3);
-  TEST_ASSERT_EQUAL_PTR(test_stack.data[2], (FFSM_State_t)2);
+  TEST_ASSERT_EQUAL(                 -1, FFSM_Stack_push(&test_stack, (FFSM_State_t)3));
+  TEST_ASSERT_EQUAL(                  3, test_stack.top);
+  TEST_ASSERT_EQUAL_PTR((FFSM_State_t)2, test_stack.data[2]);
+
   // make sure the size didn't get touched by anyone
-  TEST_ASSERT_EQUAL(test_stack.size, TEST_STACK_SIZE);
-
+  TEST_ASSERT_EQUAL(TEST_STACK_SIZE, test_stack.size);
 
   // pop tests
-  TEST_ASSERT_EQUAL_PTR(FFSM_Stack_pop(&test_stack), (FFSM_State_t)2);
-  TEST_ASSERT_EQUAL(test_stack.top, 2);
-  TEST_ASSERT_EQUAL_PTR(FFSM_Stack_pop(&test_stack), (FFSM_State_t)1);
-  TEST_ASSERT_EQUAL(test_stack.top, 1);
-  TEST_ASSERT_EQUAL_PTR(FFSM_Stack_pop(&test_stack), test_root);
-  TEST_ASSERT_EQUAL(test_stack.top, 0);
-  TEST_ASSERT_EQUAL_PTR(FFSM_Stack_pop(&test_stack), test_root);
-  TEST_ASSERT_EQUAL(test_stack.top, 0);
-  TEST_ASSERT_EQUAL_PTR(FFSM_Stack_pop(&test_stack), test_root);
+  TEST_ASSERT_EQUAL_PTR((FFSM_State_t)2, FFSM_Stack_pop(&test_stack));
+  TEST_ASSERT_EQUAL(                  2, test_stack.top);
+  TEST_ASSERT_EQUAL_PTR((FFSM_State_t)1, FFSM_Stack_pop(&test_stack));
+  TEST_ASSERT_EQUAL(                  1, test_stack.top);
+  TEST_ASSERT_EQUAL_PTR(      test_root, FFSM_Stack_pop(&test_stack));
+  TEST_ASSERT_EQUAL(                  0, test_stack.top);
+  TEST_ASSERT_EQUAL_PTR(      test_root, FFSM_Stack_pop(&test_stack));
+  TEST_ASSERT_EQUAL(                  0, test_stack.top);
+  TEST_ASSERT_EQUAL_PTR(      test_root, FFSM_Stack_pop(&test_stack));
 
   TEST_ASSERT_EQUAL_PTR(FFSM_Stack_pop((FFSM_Stack_t *)0), FFSM_STATE_NONE);
 
